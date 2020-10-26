7b6837e160f93bbc7c083d690b83e140d49794fe
Josemberg Farias Ferreira <josembergff@gmail.com>
2020-10-26 19:05:52 -0300

    Exemplo de impressão.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,9 +40,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(381, 170);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(50, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Usuário:";
             // 
             // textBox1
             // 
@@ -56,9 +56,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(381, 219);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.Size = new System.Drawing.Size(42, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Senha:";
             // 
             // textBox2
             // 
@@ -73,7 +73,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Entrar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- Form2.cs
+++ Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Text;
 using System.Windows.Forms;
 using WindowsFormsApp1.Models;
@@ -17,12 +18,66 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
             this.sqLiteContexto = sqLiteContexto;
+
+            try
+            {
+                PrintDocument printDoc = new PrintDocument();
+                printDoc.PrinterSettings.PrinterName = "Bematech 4200";
+                printDoc.DocumentName = "Cupom";
+
+                if (!printDoc.PrinterSettings.IsValid)
+                {
+                    string message = $"Não foi possível localizar a impressora!";
+                    string caption = "Falha na impressão!";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    DialogResult result = MessageBox.Show(message, caption, buttons);
+                    if (result == DialogResult.OK)
+                    {
+                        Hide();
+                        abrirForm1();
+                    }
+                }
+                else
+                {
+                    printDoc.PrintPage += new PrintPageEventHandler(PrintPageFicha);
+                    printDoc.Print();
+                }
+
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
         }
 
         private void Form2_FormClosed(object sender, FormClosedEventArgs e)
         {
+            abrirForm1();
+        }
+
+        private void abrirForm1()
+        {
             var formulario = new Form1(sqLiteContexto);
             formulario.Show();
+        }
+
+        private static void PrintPageFicha(object sender, PrintPageEventArgs ev)
+        {
+            Font titleFont = new System.Drawing.Font("Segoe UI", 17f, FontStyle.Bold);
+            Font pdvFont = new System.Drawing.Font("Segoe UI", 7f, FontStyle.Regular);
+            Font obsFont = new System.Drawing.Font("Segoe UI", 7f, FontStyle.Regular);
+
+            SizeF size = new SizeF();
+            float currentUsedHeight = 0f;
+
+            ev.Graphics.DrawString("PDV : " + "Constante Usuário Berg", pdvFont, Brushes.Black, 15, currentUsedHeight, new StringFormat());
+            size = ev.Graphics.MeasureString("PDV : " + "Constante Usuário Berg", pdvFont);
+            currentUsedHeight += size.Height;
+
+            ev.Graphics.DrawString("VENDA : Cartão", pdvFont, Brushes.Black, 15, currentUsedHeight, new StringFormat());
+            size = ev.Graphics.MeasureString("VENDA", pdvFont);
+            currentUsedHeight += size.Height;
         }
     }
 }
